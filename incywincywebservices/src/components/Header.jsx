26b717ce2a0c69c84f4383da219a7f4f93e2e0ae
Mbutler1991 <147847873+Mbutler1991@users.noreply.github.com>
2024-10-21 17:49:11 +0100

    fixed broken link in header and increased h1 size

--- incywincywebservices/src/components/Header.jsx
+++ incywincywebservices/src/components/Header.jsx
@@ -11,11 +11,11 @@ const Header = () => {
     return (
         <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900/0">
             <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr,3fr,1fr]">
-                <h1 className="hidden md:block">
+                <p className="hidden md:block">
                     <a href="/" className="logo">
                         <img src="/images/spider-web.svg" className="ml-0 mt-10" width={120} height={120} alt="IncyWincy Web Services"/>
                     </a>
-                </h1>
+                </p>
                 <div className="relative md:justify-self-center">
                     <button className="menu-btn md:hidden" onClick={toggleNavbar}>
                         <span className="material-symbols-rounded">menu</span>
@@ -24,9 +24,9 @@ const Header = () => {
                         <Navbar />
                     </div>
                 </div>
-                <a href="#home" className="hidden md:block text-purple">
+                <h1 className="hidden md:block text-purple text-2xl">
                     IncyWincy Web Services
-                </a>
+                </h1>
             </div>
         </header>
     );
