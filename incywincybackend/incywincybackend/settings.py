7e1604e7a7c66584dfb3690b80b6ba1879baf8a5
Mbutler1991 <147847873+Mbutler1991@users.noreply.github.com>
2024-10-24 17:21:28 +0100

    removed conflicting settings

--- incywincybackend/incywincybackend/settings.py
+++ incywincybackend/incywincybackend/settings.py
@@ -85,12 +85,6 @@ CORS_ALLOWED_ORIGINS = [
     "https://incywincywebservices.com",
 ]
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
