a41aa69cdfb55d864ccd124dbe788c21fbbcdb91
Mbutler1991 <147847873+Mbutler1991@users.noreply.github.com>
2024-10-24 21:39:05 +0100

    fixing issues with vite building correctly

--- incywincybackend/settings.py
+++ incywincybackend/settings.py
@@ -50,7 +50,7 @@ ROOT_URLCONF = 'incywincybackend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'incywincywebservices', 'dist')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
