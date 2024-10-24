58ba66461233ba42f96682c128e0278d1df729f6
Mbutler1991 <147847873+Mbutler1991@users.noreply.github.com>
2024-10-24 20:31:13 +0100

    updating settings for react

--- incywincybackend/settings.py
+++ incywincybackend/settings.py
@@ -128,6 +128,11 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+REACT_APP_DIR = os.path.join(BASE_DIR, 'incywincywebservices', 'dist')
+STATICFILES_DIRS = [
+    os.path.join(REACT_APP_DIR)  # Vite's build output
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
